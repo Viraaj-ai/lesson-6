81812db997ee89e3941439dbb7e3345ec0ad415c
Viraaj-ai <vintejvir@gmail.com>
2025-04-11 15:43:29 +0530

    BMI

---
+++ not operator.py
@@ -0,0 +1,4 @@
+a=500
+b=250
+if a!=b:
+    print("a and b are not equal")
---
+++ number checking.py
@@ -0,0 +1,10 @@
+a=15
+b=-10
+if a>0 and b>0:
+    print("the numbers are greater than 0")
+else:
+    print("atlest one of the numbers is lower in value than 0")
+if a>0 or b>0:
+    print("atleast one of the numbers is greater than 0")
+else:
+    print("both numbers are lesser than 0")
